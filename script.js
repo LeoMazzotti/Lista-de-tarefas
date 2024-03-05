c9fce8f06a1a4741dd4ed00c36720b3c96979dde
Leonardo Mazzotti <leonardo.mazzotti@yahoo.com.br>
2024-03-05 15:15:45 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,12 @@
+$('form').on('submit', function (e) {
+  e.preventDefault()
+
+  const cadastrarTarefa = $('#input-task').val()
+
+  const exibirTarefa = $(`<li> ${cadastrarTarefa} </li>`)
+  exibirTarefa.appendTo('ul')
+
+  $('li').click(function () {
+    $(this).toggleClass('done')
+  })
+})
